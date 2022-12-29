6d78a6a2a4ad4114f414b425fa60122ac08b1295
ilona1009 <maliarenko.ilona@gmail.com>
2022-12-29 11:07:52 +0100

    Update 1.js
    add

--- js/1.js
+++ js/1.js
@@ -886,6 +886,23 @@
 //   }
 // ]
 
+// const sortByName = users => {
+//     return [...users]
+//     .sort((a, b) => a.name.localeCompare(b.name))
+
+// };
+
+// console.log(sortByName(users))
+
+// // --------------------------------------------------------/ const sortByDescendingFriendCount = users => [...users].sort((a,b) => b.friends.length - a.friends.length)
+
+// // const sortByDescendingFriendCount = users => users.flatMap(user => user.friends)
+// //     .sort((a, b) => b.length - a.length)
+
+//     console.log(sortByDescendingFriendCount(users))
+
+// ----------------------------------------------------------
+
 // const getFriends = (users) => {
 
 //     return users
@@ -940,76 +957,33 @@
 
 // =============================================================
 
-const user = 
-[
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male"
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female"
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male"
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female"
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    gender: "male"
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    gender: "male"
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female"
-  }
-    ]
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
 
-const getTotalFriendCount = users => users.flatMap(user => user.friends)
-    .reduce((total, user) => { 
-  return total + user.friends}
-    , 0)
+// const sortedByAuthorName = [...books].sort((a,b) => a.author.localeCompare(b.author));
 
-    console.log(getTotalFriendCount)
+// const sortedByReversedAuthorName = [...books].sort((a,b) => b.author.localeCompare(a.author));
+
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+
+// const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+
+// ============================================================
