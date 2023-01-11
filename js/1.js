686f5cdd2ff217801cfe8c3b7efd2f37e0737598
ilona1009 <maliarenko.ilona@gmail.com>
2023-01-11 14:02:36 +0100

    Update 1.js
    add

--- js/1.js
+++ js/1.js
@@ -1205,3 +1205,71 @@ const users = [
 
 // console.log(getAverageAge(students));
 
+// =====================================================
+
+// function unique(arr) {
+//    return arr.filter(( el, i, array) => array.indexOf(el) === i)
+ 
+// }
+
+// let strings = ["Привіт", "Світ", "Привіт", "Світ",
+//   "Привіт", "Привіт", "Світ", "Світ", ":-O"
+// ];
+
+// console.log( unique(strings) );
+
+// ========================================================
+// let students = [
+//   {id: 'іван', name: "Іван Іванко", age: 20},
+//   {id: 'ганна', name: "Ганна Іванко", age: 24},
+//   {id: 'петро', name: "Петро Петренко", age: 31},
+// ];
+
+// const groupById = students => students.reduce((ob, student) => {
+//   ob[student.id] = student;
+//   return ob;
+// }, {})
+
+// console.log(groupById(students));
+
+// =====================================================
+
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+
+// const getTags = tweets =>
+//   tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
+
+//     return allTags;
+//   }, []);
+
+// const tags = getTags(tweets);
+// console.log(tags)
+
+// // Винесемо callback-функцію окремо, а в reducе передамо посилання на неї.
+// // Це стандартна практика, якщо callback-функція досить велика.
+
+// // Якщо в об'єкті-акумуляторі acc відсутня своя властивість з ключем tag,
+// // то створюємо її і записуємо їй значення 0.
+// // В іншому випадку збільшуємо значення на 1.
+// const getTagStats = (acc, tag) => {
+//   if (!acc.hasOwnProperty(tag)) {
+//     acc[tag] = 0;
+//   }
+
+//   acc[tag] += 1;
+
+//   return acc;
+// };
+
+// // Початкове значення акумулятора - це порожній об'єкт {}
+// const countTags = tags => tags.reduce(getTagStats, {});
+
+// const tagCount = countTags(tags);
+// console.log(tagCount);
